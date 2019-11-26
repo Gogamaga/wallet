1b7cf347474621545a4cf9637caeae2bab3341b6
Oleh <oleh.nesterenko@cyberchisel.com>
2019-11-26 16:59:20 +0200

    added filter

--- src/app/services/balance.service.ts
+++ src/app/services/balance.service.ts
@@ -12,15 +12,19 @@ export class BalanceService {
   list: any[] = [];
   selectedItem: any = {};
 
-  
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient) {
+  }
+
   getList() {
     this.http.get('http://localhost:3000/posts').subscribe((list: any[]) => this.list = list);
   }
+
   deleteItem(id: number) {
     this.http.delete(`http://localhost:3000/posts/${id}`)
-      .subscribe(() => this.list =  this.list.filter((item: any) => item.id !== id));
+      .subscribe(() => this.list = this.list.filter((item: any) => item.id !== id));
   }
+
   updateItem(data: any) {
     this.http.put(`http://localhost:3000/posts/${data.id}`, data)
       .subscribe(() => {
@@ -32,21 +36,34 @@ export class BalanceService {
         });
       });
   }
+
   createItem(data: any) {
     this.http.post(`http://localhost:3000/posts`, data)
       .subscribe((result: any) => {
         this.list.push({...data, ...result});
       });
   }
+
   selectItem(item: any) {
     this.selectedItem = item;
   }
+
+  selectFilter(item: string) {
+    this.http.get('http://localhost:3000/posts').subscribe((list: any[]) => {
+      if (item === 'all') {
+        this.list = list;
+      } else {
+        this.list = list.filter(listItem => listItem.type === item)
+      }
+    });
+  }
+
 //   toggle(){
 //     this.condition=!this.condition;
 // }
   // expres(){
   //   this.expression=!this.expression;
   //   console.log(this.expression);
-    
+
   // }
 }
